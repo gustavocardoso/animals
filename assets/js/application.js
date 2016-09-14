7a5c3c21085d9e1255f54642f1ef0700be13f46f
Gustavo Cardoso <gustavocardoso@gmail.com>
2016-09-14 20:37:56 -0300

    Intro page.

---
+++ assets/js/application.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  var app = {
+    init: function init() {
+      console.log('text');
+    }
+  };
+})();
---
+++ assets/js/src/app.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  let app = {
+    init: () => {
+      console.log('text')
+    }
+  }
+})();
