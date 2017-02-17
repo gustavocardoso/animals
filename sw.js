0d62fe609368c6c5b7b7bf06205afb120b54252f
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-02-17 13:39:40 -0200

    Adjusts

--- sw.js
+++ sw.js
@@ -16,13 +16,53 @@ const animalsFiles = [
   'assets/images/animals/owl.svg',
   'assets/images/animals/pig.svg',
   'assets/images/animals/snake.svg',
-  'assets/images/animals/whale.svg'
+  'assets/images/animals/whale.svg',
+  'assets/images/backgrounds/intro-mobile.jpg',
+  'assets/images/backgrounds/intro.jpg',
+  'assets/css/app.css',
+  'assets/js/application.js',
+  'assets/fonts/icomoon.eot',
+  'assets/fonts/icomoon.svg',
+  'assets/fonts/icomoon.ttf',
+  'assets/fonts/icomoon.woff'
 ]
 
+var CacheSW = {
+  cacheVersion: '1234567890',
+
+  name: function(cacheName){
+    return 'animals-'+ cacheName + '-' + this.cacheVersion;
+  },
+
+  get: function(cache, url, opts, cacheName){
+    return cache.match(url).then(function (response) {
+      return response || fetch(url, opts).then(function(response) {
+        cache.put(url, response.clone()).then(function() {
+          CacheSW.limitCache(cacheName);
+        });
+
+        return response;
+      });
+    })
+  },
+
+  limitCache: function(cacheName){
+    var cacheLimits = [];
+    cacheLimits[CacheSW.name('dynamic')] = 30;
+    cacheLimits[CacheSW.name('images')] = 90;
+
+    if(cacheName in cacheLimits){
+      CacheSW.trimCache(cacheName, cacheLimits[cacheName]);
+    }
+  }
+}
+
 this.addEventListener('install', (event) => {
-  console.log('SW installed')
+  console.log('install')
+  var cacheName = CacheSW.name('v2')
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open(cacheName).then((cache) => {
+      CacheSW.get(cache, '/', {}, cacheName)
       return cache.addAll(animalsFiles)
     })
   )
@@ -31,5 +71,8 @@ this.addEventListener('install', (event) => {
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
+    .then((response) => {
+      console.log(response)
+    })
   );
 });
