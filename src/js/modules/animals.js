59d4b84df42ed5d075cf7b66242fc4b032325a36
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-03-01 14:52:01 -0300

    Add test audio (mp3 and ogg) and a simple test with audio sprite

--- src/js/modules/animals.js
+++ src/js/modules/animals.js
@@ -1,20 +1,254 @@
 module.exports = [
-  { name: 'Cat', image: 'cat.svg' },
-  { name: 'Cow', image: 'cow.svg' },
-  { name: 'Dog', image: 'dog.svg' },
-  { name: 'Dolphin', image: 'dolphin.svg' },
-  { name: 'Dove', image: 'dove.svg' },
-  { name: 'Duck', image: 'duck.svg' },
-  { name: 'Elephant', image: 'elephant.svg' },
-  { name: 'Frog', image: 'frog.svg' },
-  { name: 'Hen', image: 'hen.svg' },
-  { name: 'Horse', image: 'horse.svg' },
-  { name: 'Lion', image: 'lion.svg' },
-  { name: 'Macaw', image: 'macaw.svg' },
-  { name: 'Monkey', image: 'monkey.svg' },
-  { name: 'Mouse', image: 'mouse.svg' },
-  { name: 'Owl', image: 'owl.svg' },
-  { name: 'Pig', image: 'pig.svg' },
-  { name: 'Snake', image: 'snake.svg' },
-  { name: 'Whale', image: 'whale.svg' }
+  {
+    name: 'Cat',
+    image: 'cat.svg',
+    sound: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Cow',
+    image: 'cow.svg',
+    sound: {
+      word: {
+        start: 1.674,
+        end: 2.451
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Dog',
+    image: 'dog.svg',
+    sound: {
+      word: {
+        start: 3.367,
+        end: 4.184
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Dolphin',
+    image: 'dolphin.svg',
+    sound: {
+      word: {
+        start: 5.181,
+        end: 6.117
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Dove',
+    image: 'dove.svg',
+    sound: {
+      word: {
+        start: 6.914,
+        end: 7.691
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Duck',
+    image: 'duck.svg',
+    sound: {
+      word: {
+        start: 8.548,
+        end: 9.345
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Elephant',
+    image: 'elephant.svg',
+    sound: {
+      word: {
+        start: 10.202,
+        end: 11.178
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Frog',
+    image: 'frog.svg',
+    sound: {
+      word: {
+        start: 12.015,
+        end: 12.692
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Hen',
+    image: 'hen.svg',
+    sound: {
+      word: {
+        start: 13.669,
+        end: 14.326
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Horse',
+    image: 'horse.svg',
+    sound: {
+      word: {
+        start: 15.362,
+        end: 16.279
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Lion',
+    image: 'lion.svg',
+    sound: {
+      word: {
+        start: 17.116,
+        end: 17.893
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Macaw',
+    image: 'macaw.svg',
+    sound: {
+      word: {
+        start: 18.909,
+        end: 18.885
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Monkey',
+    image: 'monkey.svg',
+    sound: {
+      word: {
+        start: 20.503,
+        end: 21.419
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Mouse',
+    image: 'mouse.svg',
+    sound: {
+      word: {
+        start: 22.256,
+        end: 23.033
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Owl',
+    image: 'owl.svg',
+    sound: {
+      word: {
+        start: 23.870,
+        end: 24.588
+      },
+      sound: {
+        start: 25.544,
+        end: 26.261
+      }
+    }
+  },
+  {
+    name: 'Pig',
+    image: 'pig.svg',
+    sound: {
+      word: {
+        start: 26.939,
+        end: 27.676
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Snake',
+    image: 'snake.svg',
+    sound: {
+      word: {
+        start: 26.979,
+        end: 27.736
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  },
+  {
+    name: 'Whale',
+    image: 'whale.svg',
+    sound: {
+      word: {
+        start: 28.632,
+        end: 29.450
+      },
+      sound: {
+        start: 0,
+        end: 
+      }
+    }
+  }
 ]
