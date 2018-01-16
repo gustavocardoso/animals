3d49e5a913961e10e840c7dc2b9406a7be0dd2e2
Gustavo Cardoso <gustavocardoso@gmail.com>
2018-01-16 02:01:09 -0200

    Add eslint; fix erros; upgrade webpack

--- src/js/modules/screen.js
+++ src/js/modules/screen.js
@@ -1,37 +1,35 @@
 export default class Screen {
-
-  constructor() {
+  constructor () {
     this.startScreen = 'intro'
   }
 
-  init() {
+  init () {
     this.setCurrentScreen(this.startScreen)
     document.querySelector('.' + this.startScreen).classList.add('active')
   }
 
-  start() {
+  start () {
     this.hideScreen(this.getCurrentScreen())
     this.setCurrentScreen('app')
     this.showCurrentScreen()
   }
 
-  setCurrentScreen(screen) {
+  setCurrentScreen (screen) {
     this.currentScreen = screen
   }
 
-  getCurrentScreen() {
+  getCurrentScreen () {
     return this.currentScreen
   }
 
-  showCurrentScreen() {
+  showCurrentScreen () {
     let screen = this.getCurrentScreen()
     document.querySelector('.' + screen).classList.add('active')
     this.setCurrentScreen(screen)
   }
 
-  hideScreen(screen) {
+  hideScreen (screen) {
     let current = screen
     document.querySelector('.' + current).classList.remove('active')
   }
-
 }
