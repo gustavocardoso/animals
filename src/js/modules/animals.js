b68fd15a72ef041dad895280e60671ab5e1d9534
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-01-19 02:03:29 -0800

    Delete js and css compiled folder and add the sources

---
+++ src/js/modules/animals.js
@@ -0,0 +1,352 @@
+export default [
+  {
+    name: 'Bear',
+    file: 'bear',
+    audio: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 0,
+        end: 3.000
+      }
+    }
+  },
+  {
+    name: 'Bird',
+    file: 'bird',
+    audio: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 4.041,
+        end: 7.800
+      }
+    }
+  },
+  {
+    name: 'Camel',
+    file: 'camel',
+    audio: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 8.788,
+        end: 11.284
+      }
+    }
+  },
+  {
+    name: 'Cat',
+    file: 'cat',
+    audio: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 12.000,
+        end: 15.002
+      }
+    }
+  },
+  {
+    name: 'Chicken',
+    file: 'chicken',
+    audio: {
+      word: {
+        start: 0,
+        end: 0.757
+      },
+      sound: {
+        start: 16.005,
+        end: 17.829
+      }
+    }
+  },
+  {
+    name: 'Cow',
+    file: 'cow',
+    audio: {
+      word: {
+        start: 1.674,
+        end: 2.451
+      },
+      sound: {
+        start: 18.868,
+        end: 21.205
+      }
+    }
+  },
+  {
+    name: 'Dog',
+    file: 'dog',
+    audio: {
+      word: {
+        start: 3.367,
+        end: 4.184
+      },
+      sound: {
+        start: 21.996,
+        end: 23.737
+      }
+    }
+  },
+  {
+    name: 'Dolphin',
+    file: 'dolphin',
+    audio: {
+      word: {
+        start: 5.181,
+        end: 6.117
+      },
+      sound: {
+        start: 24.444,
+        end: 26.260
+      }
+    }
+  },
+  {
+    name: 'Dove',
+    file: 'dove',
+    audio: {
+      word: {
+        start: 5.181,
+        end: 6.117
+      },
+      sound: {
+        start: 26.993,
+        end: 28.380
+      }
+    }
+  },
+  {
+    name: 'Duck',
+    file: 'duck',
+    audio: {
+      word: {
+        start: 8.548,
+        end: 9.345
+      },
+      sound: {
+        start: 29.285,
+        end: 30.440
+      }
+    }
+  },
+  {
+    name: 'Elephant',
+    file: 'elephant',
+    audio: {
+      word: {
+        start: 10.202,
+        end: 11.178
+      },
+      sound: {
+        start: 31.278,
+        end: 32.470
+      }
+    }
+  },
+  {
+    name: 'Frog',
+    file: 'frog',
+    audio: {
+      word: {
+        start: 12.015,
+        end: 12.692
+      },
+      sound: {
+        start: 33.451,
+        end: 35.700
+      }
+    }
+  },
+  {
+    name: 'Goat',
+    file: 'goat',
+    audio: {
+      word: {
+        start: 13.669,
+        end: 14.326
+      },
+      sound: {
+        start: 36.500,
+        end: 37.936
+      }
+    }
+  },
+  {
+    name: 'Goose',
+    file: 'goose',
+    audio: {
+      word: {
+        start: 13.669,
+        end: 14.326
+      },
+      sound: {
+        start: 38.792,
+        end: 40.380
+      }
+    }
+  },
+  {
+    name: 'Hamster',
+    file: 'hamster',
+    audio: {
+      word: {
+        start: 41.300,
+        end: 42.770
+      },
+      sound: {
+        start: 41.333,
+        end: 42.860
+      }
+    }
+  },
+  {
+    name: 'Horse',
+    file: 'horse',
+    audio: {
+      word: {
+        start: 15.362,
+        end: 16.279
+      },
+      sound: {
+        start: 44.000,
+        end: 47.235
+      }
+    }
+  },
+  {
+    name: 'Lion',
+    file: 'lion',
+    audio: {
+      word: {
+        start: 17.116,
+        end: 17.893
+      },
+      sound: {
+        start: 48.030,
+        end: 51.025
+      }
+    }
+  },
+  {
+    name: 'Macaw',
+    file: 'macaw',
+    audio: {
+      word: {
+        start: 18.909,
+        end: 19.846
+      },
+      sound: {
+        start: 52.004,
+        end: 53.700
+      }
+    }
+  },
+  {
+    name: 'Monkey',
+    file: 'monkey',
+    audio: {
+      word: {
+        start: 20.503,
+        end: 21.419
+      },
+      sound: {
+        start: 54.500,
+        end: 55.958
+      }
+    }
+  },
+  {
+    name: 'Mouse',
+    file: 'mouse',
+    audio: {
+      word: {
+        start: 22.256,
+        end: 23.033
+      },
+      sound: {
+        start: 56.935,
+        end: 60.033
+      }
+    }
+  },
+  {
+    name: 'Owl',
+    file: 'owl',
+    audio: {
+      word: {
+        start: 23.870,
+        end: 24.588
+      },
+      sound: {
+        start: 61.072,
+        end: 64.274
+      }
+    }
+  },
+  {
+    name: 'Pig',
+    file: 'pig',
+    audio: {
+      word: {
+        start: 25.539,
+        end: 26.244
+      },
+      sound: {
+        start: 65.287,
+        end: 67.704
+      }
+    }
+  },
+  {
+    name: 'Rooster',
+    file: 'rooster',
+    audio: {
+      word: {
+        start: 25.539,
+        end: 26.244
+      },
+      sound: {
+        start: 68.850,
+        end: 71.457
+      }
+    }
+  },
+  {
+    name: 'Sheep',
+    file: 'sheep',
+    audio: {
+      word: {
+        start: 26.979,
+        end: 27.736
+      },
+      sound: {
+        start: 72.670,
+        end: 73.813
+      }
+    }
+  },
+  {
+    name: 'Snake',
+    file: 'snake',
+    audio: {
+      word: {
+        start: 26.979,
+        end: 27.736
+      },
+      sound: {
+        start: 75.000,
+        end: 77.900
+      }
+    }
+  }
+]
---
+++ src/js/modules/screen.js
@@ -0,0 +1,35 @@
+export default class Screen {
+  constructor () {
+    this.startScreen = 'intro'
+  }
+
+  init () {
+    this.setCurrentScreen(this.startScreen)
+    document.querySelector('.' + this.startScreen).classList.add('active')
+  }
+
+  start () {
+    this.hideScreen(this.getCurrentScreen())
+    this.setCurrentScreen('app')
+    this.showCurrentScreen()
+  }
+
+  setCurrentScreen (screen) {
+    this.currentScreen = screen
+  }
+
+  getCurrentScreen () {
+    return this.currentScreen
+  }
+
+  showCurrentScreen () {
+    let screen = this.getCurrentScreen()
+    document.querySelector('.' + screen).classList.add('active')
+    this.setCurrentScreen(screen)
+  }
+
+  hideScreen (screen) {
+    let current = screen
+    document.querySelector('.' + current).classList.remove('active')
+  }
+}
