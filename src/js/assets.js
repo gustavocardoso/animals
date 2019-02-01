e34e952143a398a8411b470da0aae30184ec2924
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-01-31 19:03:01 -0800

    Remove words mp3

--- src/js/assets.js
+++ src/js/assets.js
@@ -2,7 +2,5 @@
 import '../sass/app.scss'
 
 // Audio
-import '../audio/animals.mp3'
-import '../audio/animals.ogg'
 import '../audio/animals/sounds-160.mp3'
 import '../audio/animals/sounds-160.ogg'
