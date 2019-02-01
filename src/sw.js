15dd3895af6d434b98382afe4eb2248047c4f9af
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-01-31 19:02:37 -0800

    Basic service worker

--- src/sw.js
+++ src/sw.js
@@ -1,88 +1,151 @@
-importScripts('/javascripts/vendor/sw-toolbox.js')
-console.log('entrou')
-self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
-  cache: {
-    name: 'asset-cache-v1',
-    maxEntries: 100
+const DEBUG = true
+const { assets } = global.serviceWorkerOption
+const CACHE_NAME = 'app-animals-v2'
+
+let assetsToCache = [...assets, './']
+
+assetsToCache = assetsToCache.map(path => {
+  return new URL(path, global.location.toString())
+})
+
+self.addEventListener('install', event => {
+  if (DEBUG) {
+    console.log('[SW]: Install event')
   }
-});
 
-// const animalsFiles = [
-//   '/images/animals/cat.svg',
-//   '/images/animals/cow.svg',
-//   '/images/animals/dog.svg',
-//   '/images/animals/dolphin.svg',
-//   '/images/animals/dove.svg',
-//   '/images/animals/duck.svg',
-//   '/images/animals/elephant.svg',
-//   '/images/animals/frog.svg',
-//   '/images/animals/hen.svg',
-//   '/images/animals/horse.svg',
-//   '/images/animals/lion.svg',
-//   '/images/animals/macaw.svg',
-//   '/images/animals/monkey.svg',
-//   '/images/animals/mouse.svg',
-//   '/images/animals/owl.svg',
-//   '/images/animals/pig.svg',
-//   '/images/animals/snake.svg',
-//   '/images/animals/whale.svg',
-//   '/images/backgrounds/intro-mobile.jpg',
-//   '/images/backgrounds/intro.jpg',
-//   '/stylesheets/app.css',
-//   '/javascripts/bundle.min.js',
-//   '/fonts/icomoon.eot',
-//   '/fonts/icomoon.svg',
-//   '/fonts/icomoon.ttf',
-//   '/fonts/icomoon.woff',
-//   'https://fonts.googleapis.com/css?family=Henny+Penny|Open+Sans:300,400,600,700,800'
-// ]
+  event.waitUntil(
+    global.caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(assetsToCache)
+      })
+      .then(() => {
+        if (DEBUG) {
+          console.log('[SW]: Cached assets: main', assetsToCache)
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        throw error
+      })
+  )
+})
 
-// var CacheSW = {
-//   cacheVersion: '1234567890',
+self.addEventListener('activate', event => {
+  if (DEBUG) {
+    console.log('[SW]: Service worker activate event')
+  }
 
-//   name: function(cacheName){
-//     return 'animals-'+ cacheName + '-' + this.cacheVersion;
-//   },
+  event.waitUntil(
+    global.caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName.indexOf(CACHE_NAME) === 0) {
+              return null
+            }
 
-//   get: function(cache, url, opts, cacheName){
-//     return cache.match(url).then(function (response) {
-//       return response || fetch(url, opts).then(function(response) {
-//         cache.put(url, response.clone()).then(function() {
-//           CacheSW.limitCache(cacheName);
-//         });
+            return global.caches.delete(cacheName)
+          })
+        )
+      })
+  )
+})
 
-//         return response;
-//       });
-//     })
-//   },
+self.addEventListener('message', event => {
+  switch (event.data.action) {
+    case 'skipWaiting':
+      if (self.skipWaiting) {
+        self.skipWaiting()
+        self.clients.claim()
+      }
+      break
+    default:
+      break
+  }
+})
 
-//   limitCache: function(cacheName){
-//     var cacheLimits = [];
-//     cacheLimits[CacheSW.name('dynamic')] = 30;
-//     cacheLimits[CacheSW.name('images')] = 90;
+self.addEventListener('fetch', event => {
+  // TODO: cache google fonts
+  const request = event.request
 
-//     if(cacheName in cacheLimits){
-//       CacheSW.trimCache(cacheName, cacheLimits[cacheName]);
-//     }
-//   }
-// }
+  if (DEBUG) {
+    console.log('[SW]: Fetch event')
+  }
 
-// this.addEventListener('install', (event) => {
-//   console.log('install')
-//   var cacheName = CacheSW.name('v2')
-//   event.waitUntil(
-//     caches.open(cacheName).then((cache) => {
-//       CacheSW.get(cache, '/', {}, cacheName)
-//       return cache.addAll(animalsFiles)
-//     })
-//   )
-// })
+  // Ignore not GET request.
+  if (request.method !== 'GET') {
+    if (DEBUG) {
+      console.log(`[SW]: Ignore non GET request ${request.method}`)
+    }
 
-// this.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//     .then((response) => {
-//       console.log(response)
-//     })
-//   );
-// });
+    return
+  }
+
+  const requestUrl = new URL(request.url)
+
+  // Ignore difference origin.
+  if (requestUrl.origin !== location.origin) {
+    if (DEBUG) {
+      console.log(`[SW]: Ignore difference origin ${requestUrl.origin}`)
+    }
+
+    return
+  }
+
+  const resource = global.caches.match(request)
+    .then(response => {
+      if (response) {
+        if (DEBUG) {
+          console.log(`[SW]: fetch URL ${requestUrl.href} from cache`)
+        }
+
+        return response
+      }
+
+      // Load and cache known assets
+      return fetch(request)
+        .then(responseNetwork => {
+          if (!responseNetwork || !responseNetwork.ok) {
+            if (DEBUG) {
+              console.log(
+                `[SW] URL [${requestUrl.toString()}] wrong responseNetwork: ${
+                  responseNetwork.status
+                } ${responseNetwork.type}`
+              )
+            }
+
+            return responseNetwork
+          }
+
+          if (DEBUG) {
+            console.log(`[SW]: URL ${requestUrl.href} fetched`)
+          }
+
+          const responseCache = responseNetwork.clone()
+
+          global.caches
+            .open(CACHE_NAME)
+            .then(cache => {
+              return cache.put(request, responseCache)
+            })
+            .then(() => {
+              if (DEBUG) {
+                console.log(`[SW]: Cache asset: ${requestUrl.href}`)
+              }
+            })
+
+          return responseNetwork
+        })
+        .catch(() => {
+          // User is landing on our page.
+          if (event.request.mode === 'navigate') {
+            return global.caches.match('./')
+          }
+
+          return null
+        })
+    })
+
+    event.respondWith(resource)
+})
+
